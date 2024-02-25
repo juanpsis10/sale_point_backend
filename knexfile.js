d6727348c9a80b60862c1cf250010cbf7ba790e9
juanpsis10 <i1320671@continental.edu.pe>
2024-02-25 11:01:19 -0500

    actualizando db

--- knexfile.js
+++ knexfile.js
@@ -2,10 +2,10 @@ module.exports = {
   development: {
     client: "mysql",
     connection: {
-      host: "b1hyosbg0tzqvnlaovej-mysql.services.clever-cloud.com",
+      host: "bvtpgx1a6ykkoaypyews-mysql.services.clever-cloud.com",
       user: "ujz6060i9a4igue9",
       password: "I3KPG9ceEv4frfeVlPAD",
-      database: "b1hyosbg0tzqvnlaovej",
+      database: "bvtpgx1a6ykkoaypyews",
     },
     useNullAsDefault: true,
   },
--- src/db.js
+++ src/db.js
@@ -1,10 +1,10 @@
 const knex = require("knex")({
   client: "mysql",
   connection: {
-    host: "b1hyosbg0tzqvnlaovej-mysql.services.clever-cloud.com",
+    host: "bvtpgx1a6ykkoaypyews-mysql.services.clever-cloud.com",
     user: "ujz6060i9a4igue9",
     password: "I3KPG9ceEv4frfeVlPAD",
-    database: "b1hyosbg0tzqvnlaovej",
+    database: "bvtpgx1a6ykkoaypyews",
   },
 });
 
