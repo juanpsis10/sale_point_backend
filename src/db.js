8c262a805ff65bf7dd3b5012147d66cb469405c8
juanpsis10 <i1320671@continental.edu.pe>
2024-03-02 03:01:42 -0500

    subiendo a producion con conecion a base de datos original

--- src/db.js
+++ src/db.js
@@ -1,10 +1,10 @@
 const knex = require("knex")({
   client: "mysql",
   connection: {
-    host: "lgg2gx1ha7yp2w0k.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    user: "gy9dc1scso3xb8nu",
-    password: "pfqfyiblhdvapi40",
-    database: "sef7yigzfi9x4jy8",
+    host: "lolyz0ok3stvj6f0.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "xglumvigvg1d8rid",
+    password: "kaz6104uner10fg4",
+    database: "bnnbiw8lldekb1ot",
   },
 });
 
